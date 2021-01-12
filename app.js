e2784230216e310d03f5a2ff176a5e4451c80be9
Danish Malik <danish.malik@Danishs-MBP.cable.rcn.com>
2021-01-12 08:55:51 -0500

    clean up

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const express = require("express");
 const cron = require("node-cron");
 const lighthouse = require('lighthouse');
@@ -15,7 +14,7 @@ const urls = [
 async function runReport(url) {
     const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless', '--no-sandbox', '--disable-gpu', '--enable-logging'] });
     const runnerResults = await lighthouse(url, { port: chrome.port }, config);
-    setTimeout(() => {chrome.kill().catch(e => console.error(e));}, 2000);
+    setTimeout(() => { chrome.kill().catch(e => console.error(e)); }, 2000);
     console.log("Ran report for:", url)
     return runnerResults;
 }
@@ -23,12 +22,6 @@ async function runReport(url) {
 async function saveReports(urls) {
     for (const url of urls) {
         const results = await runReport(url);
-
-        // // Save HTML report locally
-        // const reportHtml = results.report;
-        // const reportFilename = url.replace(/.+\/\/|www.|\..+/g, '') + '.html'
-        // fs.writeFileSync(reportFilename, reportHtml);
-        // console.log("Saved HTML report for:", url)
 
         // Convert fetchTime to Date type
         results.lhr.fetchTime = new Date(results.lhr.fetchTime)
@@ -52,16 +45,10 @@ async function saveReports(urls) {
 const app = express();
 const port = 5000;
 
-cron.schedule("*/30 * * * *", function () {
+cron.schedule("*/15 * * * *", function () {
     console.log('---------------------');
     console.log('Running cron job');
-    saveReports(urls, err => {
-        if (err) {
-            console.log("Failed run due to", err.message);
-        } else {
-            console.log("Cron run successful");
-        }
-    });
+    saveReports(urls);
 });
 
 app.listen(port, () =>
